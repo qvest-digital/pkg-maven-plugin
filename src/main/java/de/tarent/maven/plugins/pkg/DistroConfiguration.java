9a633f0c0f0477ecfcec159cfcd3bd0ef66fcbf2
Fabian Koester <f.koester@tarent.de>
2008-05-06 10:53:49 +0000

    fix for bug [#164] The jniFiles parameter does not work
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/mvn-pkg-plugin/trunk@121 cdf71550-99e3-4430-8f9a-3e0db7cf372a

--- src/main/java/de/tarent/maven/plugins/pkg/DistroConfiguration.java
+++ src/main/java/de/tarent/maven/plugins/pkg/DistroConfiguration.java
@@ -749,7 +749,7 @@ public class DistroConfiguration
                                      parent.bundleDependencies,
                                      new HashSet());
 
-    manualDependencies = (List) merge(jniFiles, parent.jniFiles,
+    manualDependencies = (List) merge(manualDependencies, parent.manualDependencies,
                                          new ArrayList());
 
     systemProperties = (Properties) merge(systemProperties,
