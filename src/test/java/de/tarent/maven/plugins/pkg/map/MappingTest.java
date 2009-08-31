f55ff8d5d86bab5459513c3a237f1ad5fbfe4443
Robert Schuster <robert.schuster@tarent.de>
2009-08-31 15:29:16 +0000

    - added test case for mapping with multiple version ranges
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/mvn-pkg-plugin/trunk@179 cdf71550-99e3-4430-8f9a-3e0db7cf372a

---
+++ src/test/java/de/tarent/maven/plugins/pkg/map/MappingTest.java
@@ -0,0 +1,71 @@
+package de.tarent.maven.plugins.pkg.map;
+
+
+import java.lang.reflect.Field;
+
+import junit.framework.TestCase;
+
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.junit.After;
+import org.junit.Before;
+
+import de.tarent.maven.plugins.pkg.map.PackageMap;
+
+public class MappingTest extends TestCase {
+	
+	private PackageMap pm;
+
+	@Before
+	public void setUp() throws Exception {
+		pm = new PackageMap(null, null, "ubuntu_karmic", null);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		pm = null;
+	}
+
+	public void testEntryWithVersionRange()
+	{
+		Mapping m = (Mapping) getFieldValue(pm, "mapping");
+		ArtifactVersion v = null;
+		Entry e = null;
+		String expected = null;
+
+		v = new DefaultArtifactVersion("2.0");
+		e = m.getEntry("commons-collections", "commons-collections", v);
+		expected = "libcommons-collections-java";
+		assertEquals(expected, e.packageName);
+		
+		v = new DefaultArtifactVersion("2.9-beta18");
+		e = m.getEntry("commons-collections", "commons-collections", v);
+		expected = "libcommons-collections-java";
+		assertEquals(expected, e.packageName);
+		
+		v = new DefaultArtifactVersion("3.0");
+		e = m.getEntry("commons-collections", "commons-collections", v);
+		expected = "libcommons-collections3-java";
+		assertEquals(expected, e.packageName);
+	}
+	
+	Object getFieldValue(Object o, String fieldName)
+	{
+		try {
+			Field f = o.getClass().getDeclaredField(fieldName);
+			
+			f.setAccessible(true);
+			
+			return f.get(o);
+		} catch (SecurityException e) {
+			throw new IllegalStateException(e);
+		} catch (NoSuchFieldException e) {
+			throw new IllegalStateException(e);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalStateException(e);
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+	
+}
