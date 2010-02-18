4d385a6c1de1e1b5afd2627b2e514f5c662aeee7
Robert Schuster <robert.schuster@tarent.de>
2010-02-18 10:03:54 +0000

    - added proper jdom entries for jaunty and karmic - added commons-codec entry - fixed commons-configuration entry - added test for loading all the package maps
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/mvn-pkg-plugin/trunk@234 cdf71550-99e3-4430-8f9a-3e0db7cf372a

---
+++ src/test/java/de/tarent/maven/plugins/pkg/map/LoadMapsTest.java
@@ -0,0 +1,42 @@
+package de.tarent.maven.plugins.pkg.map;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class LoadMapsTest extends TestCase {
+
+	/**
+	 * Add the list of distros supported by the plugin here to ensure that all the
+	 * configuration files are parsed during the test step.
+	 */
+	static final String[] distros = {
+			"debian_etch",
+			"debian_lenny",
+			"maemo_bora",
+			"maemo_chinook",
+			"openmoko_2007.11",
+			"ubuntu_gutsy",
+			"ubuntu_hardy",
+			"ubuntu_intrepid",
+			"ubuntu_jaunty",
+			"ubuntu_karmic",
+	};
+
+	/**
+	 * Loads each package map to ensure that it can be successfully parsed.
+	 */
+	public void testLoadMaps()
+	{
+		for (String d : distros)
+		{
+			try {
+				new PackageMap(null, null, d, null);
+			} catch (MojoExecutionException e) {
+				Assert.fail(e.getLocalizedMessage());
+			}
+		}
+	}
+	
+}
