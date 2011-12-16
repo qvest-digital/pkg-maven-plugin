1a5bf704cadd4b905647187bab2ccfa188e6eef4
Robert Schuster <robert.schuster@tarent.de>
2011-12-16 15:35:22 +0000

    - added two more tests for Helper.createDependencyLine()
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/mvn-pkg-plugin/trunk@464 cdf71550-99e3-4430-8f9a-3e0db7cf372a

--- src/test/java/de/tarent/maven/plugins/pkg/RPMHelperTest.java
+++ src/test/java/de/tarent/maven/plugins/pkg/RPMHelperTest.java
@@ -2,38 +2,114 @@ package de.tarent.maven.plugins.pkg;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.tarent.maven.plugins.pkg.helper.Helper;
 import de.tarent.maven.plugins.pkg.helper.RpmHelper;
+import de.tarent.maven.plugins.pkg.map.PackageMap;
+import de.tarent.maven.plugins.pkg.map.Visitor;
 
 public class RPMHelperTest extends AbstractMvnPkgPluginTestCase{
 	Packaging packaging;
-	RpmHelper rpmPackageHelper;
+	RpmHelper helper;
 	TargetConfiguration targetConfiguration;
 	boolean previousfilefound;
 	String homedir = System.getProperty("user.home");
 	File f = new File(homedir + "/.rpmmacros");
 	File buildDir = new File("src/test/resources/target");
 	String ignorePackagingTypes = "pom";
+	List<TargetConfiguration> resolvedConfigurations;
+	
+	PackageMap packageMap;
 	
 	@Before
 	public void setUp() throws Exception{
 		super.setUp();
-		packaging = (Packaging) mockEnvironment("simplepom.xml","pkg");		
+		packaging = (Packaging) mockEnvironment("simplepom.xml","pkg");
+		
+		// Blank instance that tests can modify at will.
 		targetConfiguration = new TargetConfiguration();
 		
-		// NOTE: We *violently* create a RpmHelper instance *without* a PackageMap
-		// instance because we're not going to call methods which need one. If a test
-		// is written that needs a proper PackageMap instance fix it here.
-		rpmPackageHelper = new RpmHelper();
-		rpmPackageHelper.init(packaging, null, targetConfiguration, null);
+		// A blank list of resolvedConfigurations which can be modified at will
+		// by test cases.
+		resolvedConfigurations = new ArrayList<TargetConfiguration>();
+		
+		packageMap = new PackageMap(null, null, "default", null);
+		
+		// Helper needs a PackageMap instance and that cannot be changed afterwards. However
+		// for testing purposes we want to be able to change every bit of the implementation.
+		// For this the following instance of PackageMap delegates all calls to the 'packageMap'
+		// instance. The test implementor can then set her *own* instance to packageMap. The
+		// default used above is for those who are not interested in changing the packageMap
+		// at all.
+		PackageMap delegatingPackageMap = new PackageMap(null, null, homedir, null) {
+
+			@Override
+			public String getPackaging() {
+				return packageMap.getPackaging();
+			}
+
+			@Override
+			public String getDefaultDependencyLine() {
+				return packageMap.getDefaultDependencyLine();
+			}
+
+			@Override
+			public String getDefaultJarPath() {
+				return packageMap.getDefaultJarPath();
+			}
+
+			@Override
+			public String getDefaultJNIPath() {
+				return packageMap.getDefaultJNIPath();
+			}
+
+			@Override
+			public String getDistroLabel() {
+				return packageMap.getDistroLabel();
+			}
+
+			@Override
+			public String getRepositoryName() {
+				return packageMap.getRepositoryName();
+			}
+
+			@Override
+			public String getDefaultBinPath() {
+				return packageMap.getDefaultBinPath();
+			}
+
+			@Override
+			public boolean isDebianNaming() {
+				return packageMap.isDebianNaming();
+			}
+
+			@Override
+			public boolean hasNoPackages() {
+				return packageMap.hasNoPackages();
+			}
+
+			@Override
+			public void iterateDependencyArtifacts(Log l, Collection deps,
+					Visitor v, boolean bundleNonExisting) {
+				packageMap.iterateDependencyArtifacts(l, deps, v, bundleNonExisting);
+			}
+			
+		};
+		
+		helper = new RpmHelper();
+		helper.init(packaging, delegatingPackageMap, targetConfiguration, resolvedConfigurations);
 		previousfilefound = false;
 		if(f.exists()){
 			FileUtils.moveFile(f, new File(homedir + "/.rpmmacros_Test_backup"));
@@ -55,65 +131,144 @@ public class RPMHelperTest extends AbstractMvnPkgPluginTestCase{
 	@Test
 	public void creatingRpmmacrosfileWithoutMaintainerAndRemovingSuccessfully() throws IOException, MojoExecutionException{
 		
-		rpmPackageHelper.setBasePkgDir(new File("/"));		
-		rpmPackageHelper.createRpmMacrosFile();
+		helper.setBasePkgDir(new File("/"));		
+		helper.createRpmMacrosFile();
 		Assert.assertTrue("File not found",f.exists());
-		rpmPackageHelper.restoreRpmMacrosFileBackup(null);
+		helper.restoreRpmMacrosFileBackup(null);
 	}
 	
 	@Test
 	public void creatingRpmmacrosfileWitMaintainerAndRemovingSuccessfully() throws IOException, MojoExecutionException{
 		targetConfiguration.setMaintainer("Dummy maintainer");		
-		rpmPackageHelper.setBasePkgDir(new File("/"));		
-		rpmPackageHelper.createRpmMacrosFile();
+		helper.setBasePkgDir(new File("/"));		
+		helper.createRpmMacrosFile();
 		Assert.assertTrue(f.exists());
 		Assert.assertTrue("String not found", filecontains(f, "%_gpg_name       Dummy maintainer"));
-		rpmPackageHelper.restoreRpmMacrosFileBackup(null);
+		helper.restoreRpmMacrosFileBackup(null);
 	}
 	
 	@Test(expected=NullPointerException.class)
 	public void testCreatingRpmmacrosfileWithoutBaseDirThrowsException() throws IOException, MojoExecutionException{
-		rpmPackageHelper.createRpmMacrosFile();
+		helper.createRpmMacrosFile();
 	}
 	
 	@Test
 	public void prepareInitialDirectoriesScuccesfully() throws MojoExecutionException{
 		File tempRoot = new File("/tmp/BaseTestTemp");
 		File base = new File("/tmp/BaseTestTemp/Base");
-		rpmPackageHelper.setBasePkgDir(base);
-		rpmPackageHelper.setTempRoot(tempRoot);
-		rpmPackageHelper.prepareInitialDirectories();
+		helper.setBasePkgDir(base);
+		helper.setTempRoot(tempRoot);
+		helper.prepareInitialDirectories();
 		Assert.assertTrue(new File("/tmp/BaseTestTemp/Base").exists());
-		Assert.assertEquals(new File(rpmPackageHelper.getBasePkgDir(),"/BUILD"),rpmPackageHelper.getBaseBuildDir());
-		Assert.assertEquals(new File(rpmPackageHelper.getBasePkgDir(),"/SPECS"),rpmPackageHelper.getBaseSpecsDir());		
+		Assert.assertEquals(new File(helper.getBasePkgDir(),"/BUILD"),helper.getBaseBuildDir());
+		Assert.assertEquals(new File(helper.getBasePkgDir(),"/SPECS"),helper.getBaseSpecsDir());		
 		base.delete();
 		
 	}
 	
 	@Test(expected=NullPointerException.class)
 	public void prepareInitialDirectoriesWithoutBaseOrTempRootThrowsException() throws MojoExecutionException{
-		rpmPackageHelper.prepareInitialDirectories();
+		helper.prepareInitialDirectories();
 	}
 	@Test
 	public void generateFilelist() throws MojoExecutionException, IOException{
 		File tempdir = File.createTempFile("temp", "file");
-		rpmPackageHelper.setBaseBuildDir(tempdir.getParentFile());
-		Assert.assertTrue(rpmPackageHelper.generateFilelist().size()>0);
+		helper.setBaseBuildDir(tempdir.getParentFile());
+		Assert.assertTrue(helper.generateFilelist().size()>0);
 
 	}
 	
 	@Test
 	public void getVersionReturnsPackagingVersion(){
-		Assert.assertTrue(rpmPackageHelper.getVersion().contains(packaging.project.getVersion()));
+		Assert.assertTrue(helper.getVersion().contains(packaging.project.getVersion()));
 	}
 	
 	@Test
 	public void getDstArtifactFileReturnsgetBaseBuildDirAndgetTargetArtifactFiletoStringIfNotSet(){
 		File testTempdir = new File("/tmp/BaseTestTemp");
 		File testArtifactfile = new File("file1");
-		rpmPackageHelper.setBaseBuildDir(testTempdir);
-		rpmPackageHelper.setTargetArtifactFile(testArtifactfile);
-		Assert.assertEquals(rpmPackageHelper.getBaseBuildDir()+"/"+rpmPackageHelper.getTargetArtifactFile().toString(),rpmPackageHelper.getDstArtifactFile().toString());
+		helper.setBaseBuildDir(testTempdir);
+		helper.setTargetArtifactFile(testArtifactfile);
+		Assert.assertEquals(helper.getBaseBuildDir()+"/"+helper.getTargetArtifactFile().toString(),helper.getDstArtifactFile().toString());
 		
+	}
+	
+	/**
+	 * Tests that {@link Helper#createDependencyLine} puts the so-called
+	 * dependency line from the {@lin PackageMap} into the final dependency
+	 * line.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testCreateDependencyLine_defaultDependencyLine() throws Exception {
+		final String elString = "PackagingSessionTargetDependenczRelationshipsArtifact";
+		
+		// Creates a PackageMap which has no influence on the 
+		packageMap = new PackageMap(null, null, "default", null) {
+			@Override
+			public String getDefaultDependencyLine() {
+				return elString;
+			}
+
+			@Override
+			public boolean isDebianNaming() {
+				// Not really necessary for this test but the test string contains
+				// uppercase letters and those are not allowed for Debian names.
+				return false;
+			}
+			
+			
+		};
+		
+		targetConfiguration.setTarget("foo");
+		targetConfiguration.fixate();
+
+		String line = helper.createDependencyLine();
+		Assert.assertEquals(elString, line);
+	}
+	
+ 	/**
+	 * Tests the successful addition of resolved dependencies to the
+	 * dependency line created by {@link Helper#createDependencyLine}.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testCreateDependencyLine_resolvedDeps() throws Exception {
+		// Creates a PackageMap which has no influence on the 
+		packageMap = new PackageMap(null, null, "default", null) {
+			@Override
+			public String getDefaultDependencyLine() {
+				return "";
+			}
+			
+		};
+		
+		targetConfiguration.setTarget("foo");
+		targetConfiguration.fixate();
+
+		TargetConfiguration t1 = new TargetConfiguration("t1");
+		t1.setSection("misc");
+		t1.setPackageNameSuffix("t1");
+		t1.fixate();
+
+		TargetConfiguration t2 = new TargetConfiguration("t2");
+		t2.setSection("misc");
+		t2.setPackageNameSuffix("t2");
+		t2.fixate();
+		
+		TargetConfiguration t3 = new TargetConfiguration("t3");
+		t3.setSection("misc");
+		t3.setPackageNameSuffix("t3");
+		t3.fixate();
+		
+		// Modifies the resolvedConfiguration used by the Helper
+		resolvedConfigurations.add(t1);
+		resolvedConfigurations.add(t2);
+		resolvedConfigurations.add(t3);
+		
+		String line = helper.createDependencyLine();
+		Assert.assertEquals("dummyproject-t1, dummyproject-t2, dummyproject-t3", line);
 	}
 }
